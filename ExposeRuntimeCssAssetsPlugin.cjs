fd8694ff92dd709d7e46c288cf312207532e6454
Joel Denning <joeldenning@gmail.com>
2021-02-12 16:36:18 -0700

    Fix issue with filename templates containing contenthash. Resolves #5. (#6)
    * Fix issue with filename templates containing contenthash. Resolves #5.
    
    * Fix

--- ExposeRuntimeCssAssetsPlugin.cjs
+++ ExposeRuntimeCssAssetsPlugin.cjs
@@ -4,6 +4,8 @@ const Template = require("webpack/lib/Template");
 const { RuntimeGlobals } = require("webpack");
 const { MODULE_TYPE } = require("mini-css-extract-plugin/dist/utils");
 
+const pluginName = "SingleSpaExposeRuntimeCssAssetsPlugin";
+
 class ExposedCssRuntimeModule extends RuntimeModule {
   constructor(options) {
     super("exposed-css-runtime", 10);
@@ -23,42 +25,87 @@ module.exports = class ExposeRuntimeCssAssetsPlugin {
     this.options = options;
   }
   apply(compiler) {
-    compiler.hooks.compilation.tap("SingleSpaCssPlugin", (compilation) => {
-      compilation.hooks.afterOptimizeChunks.tap(
-        "SingleSpaCssPlugin",
-        (chunks) => {
-          chunks.forEach((chunk) => {
-            if (chunk.hasEntryModule()) {
-              let foundCssModule = false;
-              for (let module of chunk.getModules()) {
-                if (module.type === MODULE_TYPE) {
-                  foundCssModule = true;
-                  break;
+    compiler.hooks.compilation.tap(pluginName, (compilation) => {
+      compilation.hooks.contentHash.tap(pluginName, (chunk) => {
+        const { outputOptions, chunkGraph } = compilation;
+        const modules = chunkGraph.getChunkModulesIterableBySourceType(
+          chunk,
+          MODULE_TYPE
+        );
+
+        if (modules) {
+          const { hashFunction, hashDigest, hashDigestLength } = outputOptions;
+          const hash = webpack.util.createHash(hashFunction);
+
+          for (const m of modules) {
+            m.updateHash(hash, { chunkGraph });
+          }
+
+          // eslint-disable-next-line no-param-reassign
+          chunk.contentHash[MODULE_TYPE] = hash
+            .digest(hashDigest)
+            .substring(0, hashDigestLength);
+        }
+      });
+
+      compilation.hooks.afterOptimizeChunks.tap(pluginName, (chunks) => {
+        chunks.forEach((chunk) => {
+          if (chunk.hasEntryModule()) {
+            let foundCssModule = false;
+            for (let module of chunk.getModules()) {
+              if (module.type === MODULE_TYPE) {
+                foundCssModule = true;
+                break;
+              }
+            }
+
+            if (foundCssModule) {
+              if (!chunk.contentHash[MODULE_TYPE]) {
+                const { outputOptions, chunkGraph } = compilation;
+                const modules = chunkGraph.getChunkModulesIterableBySourceType(
+                  chunk,
+                  MODULE_TYPE
+                );
+
+                if (modules) {
+                  const {
+                    hashFunction,
+                    hashDigest,
+                    hashDigestLength,
+                  } = outputOptions;
+                  const hash = webpack.util.createHash(hashFunction);
+
+                  for (const m of modules) {
+                    m.updateHash(hash, { chunkGraph });
+                  }
+
+                  // eslint-disable-next-line no-param-reassign
+                  chunk.contentHash[MODULE_TYPE] = hash
+                    .digest(hashDigest)
+                    .substring(0, hashDigestLength);
                 }
               }
 
-              if (foundCssModule) {
-                compilation.addRuntimeModule(
-                  chunk,
-                  new webpack.runtime.GetChunkFilenameRuntimeModule(
-                    MODULE_TYPE,
-                    "single-spa-css",
-                    `${webpack.RuntimeGlobals.require}.cssAssetFileName`,
-                    (referencedChunk) => {
-                      return this.options.filename;
-                    },
-                    true
-                  )
-                );
-                compilation.addRuntimeModule(
-                  chunk,
-                  new ExposedCssRuntimeModule({ assets: [chunk.name] })
-                );
-              }
+              compilation.addRuntimeModule(
+                chunk,
+                new webpack.runtime.GetChunkFilenameRuntimeModule(
+                  MODULE_TYPE,
+                  "single-spa-css",
+                  `${webpack.RuntimeGlobals.require}.cssAssetFileName`,
+                  (referencedChunk) => {
+                    return this.options.filename;
+                  },
+                  true
+                )
+              );
+              compilation.addRuntimeModule(
+                chunk,
+                new ExposedCssRuntimeModule({ assets: [chunk.name] })
+              );
             }
-          });
-        }
-      );
+          }
+        });
+      });
     });
   }
 };
